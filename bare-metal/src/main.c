4015398b543308bad7442ab259b7a940d78226fc
Krishnan Shankar <krishnans2006@gmail.com>
2025-09-22 16:37:56 -0500

    Improve bare-metal

--- bare-metal/src/main.c
+++ bare-metal/src/main.c
@@ -2,25 +2,18 @@
 
 extern unsigned int _sbss, _ebss, _sidata, _sdata, _edata;
 
+void __attribute__((noreturn)) call_start_cpu0() {
+    memset(&_sbss, 0, (&_ebss - &_sbss) * sizeof(_sbss));
+    memmove(&_sdata, &_sidata, (&_edata - &_sdata) * sizeof(_sdata));
+
+    main();
+}
+
 static volatile int a = 0;
+
 int main(void) {
-    // Increment a variable.
     while (1) {
         ++a;
     }
     return 0;
-}
-
-// Startup logic; this is the application entry point.
-void __attribute__((noreturn)) call_start_cpu0() {
-    // Clear BSS.
-    memset(&_sbss, 0, (&_ebss - &_sbss) * sizeof(_sbss));
-    // Copy initialized data.
-    memmove(&_sdata, &_sidata, (&_edata - &_sdata) * sizeof(_sdata));
-
-    // Done, branch to main
-    main();
-    // (Should never be reached)
-    while (1) {
-    }
 }
